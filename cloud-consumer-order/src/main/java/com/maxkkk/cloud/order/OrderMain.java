a57d40bd4cb42565b219bfcf0d6e2fc171d0e37b
shengweijun <shgnweijun@doctorcom.com>
2020-06-11 10:48:35 +0800

    1.打包成jar 2.hystix

--- cloud-consumer-order/src/main/java/com/maxkkk/cloud/order/OrderMain.java
+++ cloud-consumer-order/src/main/java/com/maxkkk/cloud/order/OrderMain.java
@@ -3,12 +3,10 @@ package com.maxkkk.cloud.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import com.maxkkk.cloud.rules.RandomRule;
-
-@RibbonClient(name = "CLOUD-PROVIDER-PAYMENT", configuration = RandomRule.class)
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
--- cloud-consumer-order/src/main/java/com/maxkkk/cloud/order/config/PaymentServiceHystix.java
+++ cloud-consumer-order/src/main/java/com/maxkkk/cloud/order/config/PaymentServiceHystix.java
@@ -5,14 +5,18 @@ import org.springframework.stereotype.Component;
 import com.maxkkk.cloud.order.entity.Payment;
 import com.maxkkk.cloud.order.service.PaymentFigenService;
 import com.maxkkk.commons.entity.CommonResult;
+import com.maxkkk.commons.enums.ResultCode;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class PaymentServiceHystix implements PaymentFigenService{
 
 	@Override
 	public CommonResult<Payment> getPaymentById(Long id) {
-		System.out.println("123");
-		return null;
+		log.info("调用断开啦");
+		return new CommonResult<Payment>(ResultCode.FAILED, null);
 	}
 
 	@Override
