ed71ccfdfff33ea4f46f6d95d3c1e40a2c606d37
shengweijun <shgnweijun@doctorcom.com>
2020-06-12 15:53:19 +0800

    1.加入hutool 2.新增gatewant网关 3.修改springcloud版本为H.SR1,H.SR5的网关gateway有BUG

---
+++ cloud-service-gateway/src/main/java/com/maxkkk/cloud/gateway/GatewayMain.java
@@ -0,0 +1,15 @@
+package com.maxkkk.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayMain {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(GatewayMain.class, args);
+	}
+
+}
---
+++ cloud-service-gateway/src/main/java/com/maxkkk/cloud/gateway/config/GatewayConfig.java
@@ -0,0 +1,17 @@
+package com.maxkkk.cloud.gateway.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration
+public class GatewayConfig {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/payment/**").uri("http://localhost:8001"))
+				.build();
+	}
+}
